ac6e069ca96efa5a5a991a03e0546ffab8168a6e
mscott716 <mscott4@macalester.edu>
2025-12-02 15:56:00 -0600

    figuring out characterScreen

--- src/starWars/CharacterScreen.java
+++ src/starWars/CharacterScreen.java
@@ -17,12 +17,33 @@ public class CharacterScreen {
         title.setCenter(390, 220);
         window.add(title);
 
-        Button continueButton = new Button("Continue");
+
+
+        Image continueButton = new Image("startGameButton");
         continueButton.setCenter(390, 510);
         continueButton.setScale(1.5);
         window.add(continueButton);
 
-        continueButton.onClick(() -> {
+        // canvas.onClick(event -> {
+        //     if (startButton.contains(event.getPosition())) {
+        //         System.out.println("Start button clicked!");
+        //         // Do something, like start game
+        //         canvas.close();  // for example, close menu
+        //         new Sketch();    // launch your game
+        //     }
+        // });
+
+        window.onClick(() -> {
+            if (continueButton.contains(mouse.getPosition())) {
+                //getX + width
+                //gety +height
+                // check if mouse click is in bounds of image and if so then
+                // create inBounds helper methods
+                System.out.println("Start button clicked!");
+                // Do something, like start game
+                window.closeWindow();
+                new Sketch();
+            }
             window.closeWindow();
             new Sketch();
         });
